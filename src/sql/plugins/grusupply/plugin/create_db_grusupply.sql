ccd094214a8770b04c24b963df5b7e861545742a
ATOS-NTY <nicolas.tanqueray@atos.net>
2017-06-16 10:27:16 +0200

    GRUSUPPLY-39 - clean and rework impact

--- src/sql/plugins/grusupply/plugin/create_db_grusupply.sql
+++ src/sql/plugins/grusupply/plugin/create_db_grusupply.sql
@@ -9,17 +9,3 @@ id_customer int(11) NOT NULL ,
 id_user int(11),
 PRIMARY KEY (id_mapping)
 );
-
---
--- Structure for table grusupply_demand_indexer_action
---
-
-DROP TABLE IF EXISTS grusupply_demand_indexer_action;
-CREATE TABLE grusupply_demand_indexer_action (
-  id_action INT DEFAULT 0 NOT NULL,
-  id_demand VARCHAR(50) DEFAULT '' NOT NULL,
-  demand_type_id VARCHAR(50) DEFAULT '' NOT NULL,
-  id_task INT DEFAULT 0 NOT NULL ,
-  PRIMARY KEY (id_action)
-  );
-CREATE INDEX grusupply_demand_indexer_action ON grusupply_demand_indexer_action (id_task);
---
+++ src/sql/plugins/grusupply/upgrades/upgrade_db_grusupply-1.2.4-1.2.5.sql
@@ -0,0 +1,4 @@
+--
+-- Drop table grusupply_demand_indexer_action
+--
+DROP TABLE IF EXISTS grusupply_demand_indexer_action;
